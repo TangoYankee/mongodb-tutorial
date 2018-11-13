c9ca0330832042588ac09acea8005b5cd8fe8198
tangoyankee <miller.tim108@gmail.com>
2018-11-12 20:06:58 -0800

    object-oriented design

--- app.py
+++ app.py
@@ -1,204 +1,119 @@
-from flask import Flask
-from pymongo import MongoClient, ReturnDocument
-from validators import User, AllUsers
+"""
+Contains all views for application
+"""
 
-CHECKOUT_LIMIT = 3
+from flask import Flask
+from validators import User, Book, Library
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
-
 app.debug = True
 
-db = client.mongodb_tutorial
-## Home
+LIBRARY = Library()
 
-# default routing provies reference to all routes
 @app.route("/")
 def index():
-	return 'Please follow links to make use of Mongo Database: <br>' \
-		'1. /users/ - get all users <br>' \
-		'2. /users/username/ - get particular user <br>' \
+    """default routing provides reference to all routes"""
+    return 'Please follow links to make use of Mongo Database: <br>' \
+        '1. /users/ - get all users <br>' \
+        '2. /users/username/ - get particular user <br>' \
 
-### Users ###
-# Get all users
-@app.route("/users/")
-def get_users():
-	users = AllUsers()
-	statement = users.get
-	return statement
-
-# get a specific user by username
 @app.route("/users/get/")
 @app.route("/users/get/<username>")
 def get_user(username=None):
-	if username:
-		user = User(username)
-		statement = user.get()
-	else:
-		statement = 'please provide a username'
-	return statement
+    """get a specific user by username"""
+    if username:
+        user = User(username)
+        statement = user.get()
+    else:
+        statement = 'please provide a username'
+    return statement
 
-# create a user
-## TODO: Make username a unique field. Replace user_id with username as key from book to user
 @app.route("/users/post/<username>/<firstname>/<lastname>")
 def post_user(username=None, firstname=None, lastname=None):
-	if username:
-		user = User(username)
-		statement = user.post(firstname, lastname)
-	else:
-		statement = 'no username provided'
+    """create a user"""
+    if username:
+        user = User(username)
+        statement = user.post(firstname, lastname)
+    else:
+        statement = 'no username provided'
+    return statement
 
-	return statement
-
-# delete a user
-# TODO: Unable to delete user while books are checked out
 @app.route("/users/delete/")
 @app.route("/users/delete/<username>")
 def delete_user(username=None):
-	if username:
-		user = User(username)
-		statement = user.delete()
-	else:
-		statement = 'username missing'
-	return statement
+    """delete a user, TODO: Unable to delete user while books are checked out"""
+    if username:
+        user = User(username)
+        statement = user.delete()
+    else:
+        statement = 'username missing'
+    return statement
 
-### Books ###
-# Get all books
-@app.route("/books/")
-def get_books():
-	try:
-		books = db.books.find()
-		statement = ''
-		for book in books:
-			statement = statement + (f'{book}<br>')
-	except:
-		statement = 'an unknown error occured'
-	return statement
-
-# get a specific book
 @app.route("/books/get/")
 @app.route("/books/get/<title>")
 def get_book(title=None):
-	if title:
-		try:
-			books = db.books.find({'title':title})
-			statement = ''
-			for book in books:
-				statement = statement + f'{book}<br>'
-		except:
-			statement = 'an error occurred'
-	else:
-		statement = 'no title provided'
-	return statement
+    """get a specific book"""
+    if title:
+        book = Book(title)
+        statement = book.get()
+    else:
+        statement = 'no title provided'
+    return statement
 
-# post (create) a book
 @app.route("/books/post/")
 @app.route("/books/post/<title>/<year>")
 def post_book(title=None, year=None, borrower=None):
-	if title and year:
-		# The books collection is started automatically
-		db.books.insert_one({ 
-			"title": title,
-			"year": year,
-			"borrower": borrower
-		})
-		statement = (f'{title}, {year}, {borrower}')
-	else:
-		statement = 'invalid'
-	return statement
+    """post (create) a book"""
+    if title and year:
+        book = Book(title)
+        statement = book.post(year, borrower)
+    else:
+        statement = 'invalid'
+    return statement
 
-# delete a book
-# TODO: Programmatically manipulate a book based on UUID
-# include a check for an borrower
 @app.route("/books/delete/")
 @app.route("/books/delete/<title>")
 def delete_book(title=None):
-	if title:
-		book = db.books.find_one({"title": title}, {'borrower': 1})
-		borrower = book['borrower']
-		if not borrower:
-			try:
-				db.books.remove({
-					"title": title,
-				})
-				statement = f'{title} removed'
-			except:
-				statement = 'error'
-		else:
-			statement = f'this book is checkout by {borrower} and cannot be deleted'
-	else:
-		statement = 'title missing'
-	return statement
+    """# delete a book # TODO: Programmatically manipulate a book based on UUID"""
+    if title:
+        book = Book(title)
+        statement = book.delete()
+    else:
+        statement = 'title missing'
+    return statement
 
-# checkout a book
+@app.route("/<collection>/")
+def get_collection(collection=None):
+    """Get all items in a collection"""
+    statement = LIBRARY.get(collection)
+    return statement
+
+@app.route("/<collection>/reset")
+def reset_collections(collection=None):
+    '''Delete all items in a collection'''
+    statement = LIBRARY.reset(collection)
+    return statement
+
+
 @app.route("/checkout/")
 @app.route("/checkout/<title>/<username>")
-def checkout(title = None, username = None):
-	if title and username:
-		try:
-			book = db.books.find_one_and_update(
-									{'$and': [
-										{ 'title' : title },
-										{ 'borrower': None}]},
-									{'$set' : 
-										{"borrower": username}},
-									return_document=ReturnDocument.AFTER	
-									)
-			book_id = book['_id']
-			user = db.users.update_one(
-				{"username": username},
-				{"$push": 
-					{"books": book_id}
-				}
-			)
-			statement = f'Book: {book} <br> {user}'
-		except:
-			statement = 'error'
-	else:
-		statement = 'please provide username and title'
-	return statement
+def checkout(title=None, username=None):
+    """A user checks out an available copy of a book"""
+    if title and username:
+        statement = LIBRARY.checkout(title, username)
+    else:
+        statement = 'please provide username and title'
+    return statement
 
-# checkin a book
 @app.route('/checkin/')
 @app.route('/checkin/<title>/<username>')
 def check_in(title=None, username=None):
-	if title and username:
-		try:
-			book = db.books.find_one_and_update({'$and': [
-								{ 'title' : title },
-								{ 'borrower': username}]},
-							{'$set' : 
-								{"borrower": None}},
-							return_document=ReturnDocument.AFTER)
-			book_id = book['_id']
-			user = db.users.update_one(
-				{"username": username},
-				{"$pull": {"books": book_id}})
-			statement = f'Book: {book} <br> User: {user}'
-		except:
-			statement = 'error'
-	else:
-		statement = 'please provide title and borrower'
-	return statement
-	
-
-@app.route("/users/reset")
-def reset_users():
-	try:
-		db.users.remove()
-		statement = 'users reset'
-	except:
-		statement = 'error'
-	return statement
-
-@app.route("/books/reset")
-def reset_books():
-	try:
-		db.books.remove()
-		statement = 'books reset'
-	except:
-		statement = 'error'
-	return statement
+    """A user checks in an available copy of a book"""
+    if title and username:
+        statement = LIBRARY.checkin(title, username)
+    else:
+        statement = 'please provide title and borrower'
+    return statement
 
 if __name__ == "__main__":
     app.run()
--- validators.py
+++ validators.py
@@ -1,43 +1,49 @@
-from schema import Schema, Optional
-from pymongo import MongoClient
+"""
+Handle the requests to the Mongo Database
+"""
+from schema import Schema, Optional, Or
+from pymongo import MongoClient, ReturnDocument
 
-client = MongoClient('localhost', 27017)
-db = client.mongodb_tutorial
+CLIENT = MongoClient('localhost', 27017)
+DB = CLIENT.mongodb_tutorial
 
 class User:
+    """Handle calls for users collection"""
     def __init__(self, username):
+        """username and schema are standard to every instance"""
         self.username = username
-        self.collection = db.users
         self.user_schema = Schema({
-                    "username":         str,
-                    "firstname":        str,
-                    "lastname":         str,
-                    Optional("books"):  list
-                })
+            "username":         str,
+            "firstname":        str,
+            "lastname":         str,
+            Optional("books"):  list
+            })
 
     def get(self):
+        """Retrieve values for user from database"""
         try:
-            user = self.collection.find_one({'username': self.username})
+            user = DB.users.find_one({'username': self.username})
             statement = f'{user}'
         except:
             statement = "error"
         return statement
 
     def post(self, firstname, lastname):
+        """Create validated and json formatted user document"""
         self.firstname = firstname
         self.lastname = lastname
-        self.json_data
         try:
             self.valid
-            self.collection.insert_one(self.json_data)
+            DB.users.insert_one(self.json_data)
             statement = f'{self.json_data}'
         except:
             statement = 'invalid'
         return statement
 
     def delete(self):
+        """Remove user document from collection"""
         try:
-            self.collection.remove({"username": self.username})
+            DB.users.remove({"username": self.username})
             statement = f'{self.username} removed'
         except:
             statement = 'error'
@@ -45,28 +51,141 @@ class User:
 
     @property
     def json_data(self):
-        return  {
-                    "username": self.username,
-                    "firstname": self.firstname,
-                    "lastname": self.lastname,
-                    "books": []
-                }
+        """Translate HTTP Request to JSON
+            TODO: Replace with requests"""
+        return {
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "books": []
+            }
 
     @property
     def valid(self):
+        """Check current data against established schema"""
         return self.user_schema.validate(self.json_data)
 
-class AllUsers:
-    def __init__(self):
-        self.collection = db.users
+class Book:
+    """Handle collection for books"""
+    def __init__(self, title):
+        """Title and Schema are standard to all requests"""
+        self.title = title
+        self.book_schema = Schema({
+            "title": str,
+            "year": str,
+            Optional("borrower"): Or(str, None)
+            })
+
+    def get(self):
+        """Retrieve values for book from database"""
+        try:
+            book = DB.books.find_one({'title': self.title})
+            statement = f'{book}'
+        except:
+            statement = "error"
+        return statement
+
+    def post(self, year, borrower):
+        """Create book based on preset schema"""
+        self.year = year
+        self.borrower = borrower
+        self.json_data
+        try:
+            self.valid
+            DB.books.insert_one(self.json_data)
+            statement = f'{self.json_data}'
+        except:
+            statement = 'invalid data'
+        return statement
+
+    def delete(self):
+        """Remove book, only if it is not checked out"""
+        try:
+            book = DB.books.find_one({"title": self.title}, {'borrower': 1})
+            borrower = book['borrower']
+            if not borrower:
+                try:
+                    DB.books.remove({"title": self.title})
+                    statement = f'{self.title} removed'
+                except:
+                    statement = 'removal error'
+            else:
+                statement = f'this book is checked out by {borrower}'
+        except:
+            statement = 'title error'
+        return statement
 
     @property
-    def get(self):
+    def json_data(self):
+        """Convert HTTP request into JSON format"""
+        return  {
+            "title": self.title,
+            "year": self.year,
+            "borrower": self.borrower
+            }
+
+    @property
+    def valid(self):
+        """Check against preset Schema"""
+        return self.book_schema.validate(self.json_data)
+
+class Library:
+    """Manage functions that act on all documents in a collection"""
+    def get(self, collection):
+        """get all documents from a collection"""
+        if collection == 'books':
+            collection = DB.books
+        elif collection == 'users':
+            collection = DB.users
+        else:
+            return 'please provide valid collection'
         try:
-            users = self.collection.find()
+            items = collection.find()
             statement = ''
-            for user in users:
-                statement = statement + (f'{user}<br>')
+            for item in items:
+                statement = statement + (f'{item}<br>')
         except:
             statement = 'an unknown error occured'
+        return statement
+
+    def reset(self, collection):
+        """Remove all documents from a collection"""
+        if collection == 'books':
+            collection = DB.books
+        elif collection == 'users':
+            collection = DB.users
+        else:
+            return 'please provide valid collection'
+        try:
+            collection.remove()
+            statement = f'{collection} reset'
+        except:
+            statement = 'error'
+        return statement
+
+    def checkin(self, title, username):
+        """Return a book to the library"""
+        try:
+            book = DB.books.find_one_and_update({
+                '$and':[{'title': title}, {'borrower': username}]}, {
+                    '$set':{"borrower": None}}, return_document=ReturnDocument.AFTER)
+            book_id = book['_id']
+            DB.users.update_one(
+                {"username": username},
+                {'$pull': {"books": book_id}})
+            statement = f'{book}'
+        except:
+            statement = 'unable'
+        return statement
+
+    def checkout(self, title, username):
+        """Assign a book to a borrower"""
+        book = DB.books.find_one_and_update({
+            '$and':[{'title' : title}, {'borrower' : None}]}, {
+                '$set' : {"borrower" : username}}, return_document=ReturnDocument.AFTER)
+        book_id = book['_id']
+        DB.users.update_one({
+            "username" : username}, {
+                "$push":{"books": book_id}})
+        statement = f'{book}'
         return statement
