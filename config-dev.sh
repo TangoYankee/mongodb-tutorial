8359176ea074a268be2370aaaa9b0300dd6e0e32
tangoyankee <miller.tim108@gmail.com>
2020-02-03 21:17:53 -0800

    username field for db

--- config-dev.sh
+++ config-dev.sh
@@ -1,3 +1,4 @@
 export FLASK_ENV=development
 export FLASK_APP=lendinglibrary
 export MONGODB_PASSWORD = ""
+export MONGODB_USERNAME = ""
--- lendinglibrary/database.py
+++ lendinglibrary/database.py
@@ -3,5 +3,5 @@ from lendinglibrary import app
 from flask_pymongo import PyMongo
 
 
-app.config["MONGO_URI"] = f"mongodb+srv://lendinglibrary:{getenv['MONGODB_PASSWORD']}@cluster0-075z2.mongodb.net/test?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = f"mongodb+srv://{getenv('MONGODB_USERNAME')}:{getenv('MONGODB_PASSWORD')}@cluster0-075z2.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
