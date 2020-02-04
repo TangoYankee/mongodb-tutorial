98fe900b3013ae7587de696678f8275847dafbf4
tangoyankee <miller.tim108@gmail.com>
2020-02-03 21:14:17 -0800

    config file before it is given an actual password

--- lendinglibrary/database.py
+++ lendinglibrary/database.py
@@ -1,6 +1,7 @@
+from os import getenv
 from lendinglibrary import app
 from flask_pymongo import PyMongo
 
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/lendingLibraryDB"
+app.config["MONGO_URI"] = f"mongodb+srv://lendinglibrary:{getenv['MONGODB_PASSWORD']}@cluster0-075z2.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
