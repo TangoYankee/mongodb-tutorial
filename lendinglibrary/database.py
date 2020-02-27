c6079f77e61795794056d0f17db488b3bc7b8b6a
tangoyankee <miller.tim108@gmail.com>
2020-02-26 16:00:48 -0800

    port and case senstive database for local development

--- lendinglibrary/database.py
+++ lendinglibrary/database.py
@@ -2,7 +2,7 @@ from os import getenv
 from lendinglibrary import app
 from flask_pymongo import PyMongo
 
-
-app.config["MONGO_URI"] = f"mongodb+srv://{getenv('MONGODB_USERNAME')}:{getenv('MONGODB_PASSWORD')}@cluster0-075z2.mongodb.net/lendinglibrarydb?retryWrites=true&w=majority"
-# app.config["MONGO_URI"] = "mongodb://localhost:8080"
+# app.config["MONGO_DBMAME"] = 'lendingLibraryDB'
+# app.config["MONGO_URI"] = f"mongodb+srv://{getenv('MONGODB_USERNAME')}:{getenv('MONGODB_PASSWORD')}@cluster0-075z2.mongodb.net/lendinglibrarydb?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/lendingLibraryDB"
 mongo = PyMongo(app)
