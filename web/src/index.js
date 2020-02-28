b51d63a2a30fd8017d9427762e75dbd63faf90e1
tangoyankee <miller.tim108@gmail.com>
2020-02-27 18:58:10 -0800

    configure testing and general app structure

--- web/src/index.js
+++ web/src/index.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App/App'
 
+// const Welcome = (props) => {
+//   return <h2 className="usa-heading heading-margin-alt">Hello, {props.name}</h2>
 
-const Welcome = (props) => {
-  return <h2 className="usa-heading heading-margin-alt">Hello, {props.name}</h2>
+// }
 
-}
+// // https://reactjs.org/docs/state-and-lifecycle.html
 
-// https://reactjs.org/docs/state-and-lifecycle.html
+// const Comment = (props) => {
+//   return <p className="site-text-intro">{props.text}</p>
+// }
 
-const Comment = (props) => {
-  return <p className="site-text-intro">{props.text}</p>
-}
-
-const App = () => {
-  return (
-    <div className="usa-prose site-prose">
-      <h6 className="usa-heading-alt">Spacing</h6>
-      <h1>Page heading</h1>
-      <Welcome name="Sara"/>
-      <Comment text="What a strange world"/>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div className="usa-prose site-prose">
+//       <h6 className="usa-heading-alt">Spacing</h6>
+//       <h1>Page heading</h1>
+//       <Welcome name="Sara"/>
+//       <Comment text="What a strange world"/>
+//     </div>
+//   )
+// }
 
 ReactDOM.render(
   <App />,
