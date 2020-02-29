e4163140b910ce3f9cc5bf996d685715654ff188
tangoyankee <miller.tim108@gmail.com>
2020-02-29 12:08:07 -0800

    https and csrf protect dev settings

--- api/jwt_config.py
+++ api/jwt_config.py
@@ -6,8 +6,9 @@ app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/v1/'
 app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/'
 
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 
+app.config['JWT_COOKIE_SECURE'] = False
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 app.config['JWT_SECRET_KEY'] = getenv('JWT_SECRET_KEY')
 
 jwt = JWTManager(app)
