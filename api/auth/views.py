9ce13b6faeab2aae0374dee3f1815b584249b478
tangoyankee <miller.tim108@gmail.com>
2020-02-29 10:58:23 -0800

    skeleton functions for authorization

---
+++ api/auth/views.py
@@ -0,0 +1,21 @@
+from api import app
+
+from flask import jsonify, request
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+# @app.route('/token/auth/', methods=['GET', 'POST'])
+def login():
+  return 'login'
+  # _json = request.json
+  # username = _json.get('username', None)
+  # password = _json.get('password', None)
+  # if username != 'test' or password != 'test':
+  #   return jsonify({'login': False}), 401
+  # access_token = create_access_token(identity=username)
+  # refresh_token = create_refresh_token(identity=username)
+
+def refresh():
+  return 'refresh'
+
+def logout():
+  return 'logout'
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from api import app
 from api.views.user_views import UserAPI
+from api.auth.views import login, refresh, logout
+from flask import request
 
 
 def register_api(view, endpoint, url, pk='id', pk_type='string'):
@@ -9,3 +11,16 @@ def register_api(view, endpoint, url, pk='id', pk_type='string'):
     app.add_url_rule('%s<%s:%s>'% (url, pk_type, pk), view_func=view_func, methods=['GET' ,'PUT' ,'DELETE' ])
     
 register_api(UserAPI,'user_api','/api/v1/users/', pk='user_id')
+
+
+# poor scheme for urls as it puts verbs into the urls
+app.add_url_rule('/token/auth/', 'token_auth', login)
+app.add_url_rule('/token/refresh/', 'token_refresh', refresh)
+app.add_url_rule('/token/remove/', 'token_remove', logout)
+
+def index():
+    return 'Hello World!'
+index.provide_automatic_options = False
+index.methods = ['GET']
+
+app.add_url_rule('/', 'index', index)
