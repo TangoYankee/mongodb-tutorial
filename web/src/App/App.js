9868c327709b69e502e00e52bfff3ab5f16a55b9
tangoyankee <miller.tim108@gmail.com>
2020-03-09 12:18:57 -0700

    header module

--- web/src/App/App.js
+++ web/src/App/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './App.css'
 import Brand from '../Brand/Brand'
 import Context from '../Context/Context'
+import Header from '../Header/Header'
 
 const App = () => {
   return (
     <div className="home">
+      <Header />
       <Brand />
       <Context />
     </div>
-
   )
 }
 
---
+++ web/src/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return(
+    <div className="header">
+      <ul>
+        <li>Books</li>
+        <li>Not Books</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
