aa870201a6cf36e52ee5c4bc2d7e8b621d48af0a
Timothy Miller <miller.tim108@gmail.com>
2020-03-11 12:20:10 -0700

    remove uswds package and dependency (#24)

--- web/src/Brand/Brand.js
+++ web/src/Brand/Brand.js
@@ -4,12 +4,12 @@ import './Brand.css'
 class Brand extends React.Component {
   render() {
     return (
-      <div className="usa-prose brand homeitem">
+      <main className="brand homeitem">
         <h1>Phoenix Spark</h1>
         <h2>Travis AFB, CA</h2>
         <img alt='phoenix with spread wings' src={`${process.env.PUBLIC_URL}/spark-logo-crop.png`} />
         <h2>Lending Library</h2>
-      </div>
+      </main>
     )
   }
 }
--- web/src/Context/Context.js
+++ web/src/Context/Context.js
@@ -20,12 +20,12 @@ class Context extends React.Component {
 
 const Mission = () => {
   return (
-    <div>
+    <aside>
       <h3>Mission</h3>
       <p>Ut molestie, neque quis varius dapibus, dolor odio commodo magna, eget accumsan nulla eros in nisl. Proin non luctus ex, eget fermentum quam. Donec pretium orci nibh, nec consectetur libero aliquet quis. Aenean tempus lorem ut nisi pharetra pellentesque. Vestibulum non urna vel leo aliquet facilisis. Proin non eleifend turpis, vitae blandit ex. Maecenas laoreet augue quis ante sagittis commodo. Ut consequat quam eu massa tincidunt, a pellentesque turpis venenatis. Pellentesque luctus ultricies tellus et egestas. In diam ex, iaculis eu ante quis, pharetra lobortis dolor. Praesent quis odio vitae lacus porta pharetra. Sed id posuere dui. </p>
       <h3>Resources</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin ante tincidunt rhoncus rhoncus. Phasellus dignissim dolor id nisl consequat, ut malesuada nisl hendrerit. Sed molestie, orci eu auctor sodales, quam tortor dictum felis, et aliquet erat tellus in tortor. Sed vel libero suscipit, tincidunt turpis vestibulum, vestibulum tellus. Phasellus ultrices enim id libero bibendum, et molestie arcu consequat. Proin eget risus eu justo pulvinar mollis nec ut risus. Maecenas quis ex ex</p>
-    </div>
+    </aside>
   )
 }
 
