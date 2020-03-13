bca7b84fb1824a3fef08511466da95f82cc527e7
Timothy Miller <miller.tim108@gmail.com>
2020-03-13 12:42:49 -0700

    Test lint setup (#33)
    * dev dependecies for tests and linting
    
    * contribute dev dependencies and linting commands

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from os import getenv
 
 from flask_jwt_extended import JWTManager
 
-if __name__=="__main__":
-  # app.run(host="0.0.0.0", port="8080")
-  # Can't run below port 1024 as an unprivileged user
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port="8080")
+    # Can't run below port 1024 as an unprivileged user
 
-  app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="8080")
